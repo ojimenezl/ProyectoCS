9e0747fdf174a36419e01f5496663cfaa871704e
ojimenezl <ojimenezl@est.ups.edu.ec>
2021-04-21 01:06:38 -0500

    primer commit sesion

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,11 @@
+const indexCtrl = {};
+
+indexCtrl.renderIndex = (req, res) => {
+  res.render('users/signin');
+};
+
+indexCtrl.renderAbout = (req, res) => {
+  res.render('about');
+};
+
+module.exports = indexCtrl;
